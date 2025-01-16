a85ddc736b6eb7448ea731eb695aa4209b1c28c3
AugustinSeguin <augustinseguin@gmail.com>
2025-01-16 11:20:57 +0100

    init npm project

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # api-exam-mds-mba1
-api-exam-mds-mba1
+
+## Créer la base de données 
+
+Via un outil comme **dbeaver**, créer la base de données SQLite
+via le fichier `db/database.sqlite`
+
+## Lancer le projet en local
+
+Installer les dépendances
+```bash
+npm install
+```
+
+Créer le fichier de config .env
+```bash
+cp .env.example .env
+```
+Rajouter les valeurs manquantes dans le fichier .env
+
+Installer les dépendances
+```bash
+npm run dev
+```
+
+L'application se lance sur le port défini dans le .env
---
+++ src/index.ts
@@ -0,0 +1,44 @@
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
+import { Sequelize } from "sequelize";
+
+import { actorRouter } from "./router/actor";
+import { movieRouter } from "./router/movie";
+import { authRouter } from "./router/auth";
+import { userRouter } from "./router/users";
+import { exportModels } from "./model/Model";
+
+export const sequelize = new Sequelize({
+  dialect: 'sqlite',
+  storage: 'db/database.sqlite'
+});
+
+// sequelize.sync({ force: true });
+sequelize.sync();
+
+export const { Movie,
+  Actor,
+  User,
+  TokenBlackList,
+  MovieActorModel } = exportModels(sequelize);
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const apiRouter = express.Router();
+apiRouter.use('/auth', authRouter);
+apiRouter.use('/movies', movieRouter);
+apiRouter.use('/actors', actorRouter);
+apiRouter.use('/users', userRouter);
+
+app.use("/api", apiRouter);
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${process.env.PORT}!`)
+});
+
+app.get("/", (req, res) => {
+  res.json({ message: "Hello World!" });
+});
